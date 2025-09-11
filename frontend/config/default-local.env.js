9f1a223cba220d4c1e6639d2ec741b470e8e26aa
Andy Juliadi <andy.juliadi@sociolla.com>
2025-09-12 01:56:23 +0700

    fix: adding new rules

--- frontend/config/default-local.env.js
+++ frontend/config/default-local.env.js
@@ -1,5 +1,5 @@
 const ENV = ENV_LOCAL;
 
 if (ENV === "local") {
-  MS_JARVIS_API_URL = "http://localhost:8990";
+  MS_SANCTUM_API_URL = `http://localhost:7002`;
 }
---
+++ frontend/config/web-local.env.js
@@ -0,0 +1,5 @@
+if (NODE_ENV === "production") {
+  // there is no changes on here
+} else if (true) {
+  MS_SANCTUM_API_URL = `http://localhost:7002`;
+}
